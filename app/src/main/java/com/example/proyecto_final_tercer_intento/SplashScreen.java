cc420e8727bad7a0a6fca435e9ffa6207da2148e
Yenifer22 <gennstacy@gmail.com>
2019-11-12 14:52:32 -0600

    Agregando el SplashScreen

---
+++ app/src/main/java/com/example/proyecto_final_tercer_intento/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_final_tercer_intento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
